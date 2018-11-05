97db8aae6231b5f0a9684108f24185dc1b1552e4
Yuki N <lunarbase@outlook.com>
2018-11-05 05:10:00 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages].join(" ")
 end
 
 def adding_matz
@@ -79,7 +81,14 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+     
+    maz = {:yukihiro_matsumoto => {
+               :known_for => "Ruby",
+               :languages => ["LISP", "C"]}}
+  
+    programmer_hash = maz.merge(programmer_hash)
+    
+    programmer_hash
     
 end
 
@@ -102,6 +111,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash
      
 end
 
@@ -121,5 +132,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+     programmer_hash
 
 end
