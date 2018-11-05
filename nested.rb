37fb7e0f655f46187ae4575d32cdefc266760731
Yuki N <lunarbase@outlook.com>
2018-11-05 04:55:44 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages].join(" ")
 end
 
 def adding_matz
@@ -79,7 +81,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+     
+     programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",}
+     
     
 end
 
